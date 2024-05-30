e3cdf6c3c069b54e63aba0681ce8df5bb4073c5f
origin <comstudynews@gmail.com>
2024-05-30 13:12:09 +0900

    03 commit day08

--- src/main/java/org/comstudy/web/controller/BoardController.java
+++ src/main/java/org/comstudy/web/controller/BoardController.java
@@ -7,16 +7,16 @@ import org.comstudy.web.Controller;
 import org.comstudy.web.ModelAndView;
 import org.comstudy.web.board.command.BoardCommand;
 
+// 게시판의 CRUD를 비롯한 세부 기능을 수행하는 Command가 분기된다.
+// 실행 되는 Command에서 DAO등의 기능을 수행하고 그 결과를 
+// ModelAndView에 addObject로 셋팅 해 준다.
+
 public class BoardController implements Controller {
+	private Command cmd = new BoardCommand();
 
 	@Override
 	public ModelAndView action(HttpServletRequest req) {
-	
-		// 게시판의 CRUD를 비롯한 세부 기능을 수행하는 Command가 분기된다.
-		// 실행 되는 Command에서 DAO등의 기능을 수행하고 그 결과를 
-		// ModelAndView에 addObject로 셋팅 해 준다.
 		
-		Command cmd = new BoardCommand();
 		ModelAndView mnv = cmd.execute(req);
 		
 		return mnv;
--- src/main/java/org/comstudy/web/controller/TodoController.java
+++ src/main/java/org/comstudy/web/controller/TodoController.java
@@ -8,16 +8,17 @@ import org.comstudy.web.ModelAndView;
 import org.comstudy.web.board.command.BoardCommand;
 import org.comstudy.web.todo.command.TodoCommand;
 
+// 게시판의 CRUD를 비롯한 세부 기능을 수행하는 Command가 분기된다.
+// 실행 되는 Command에서 DAO등의 기능을 수행하고 그 결과를 
+// ModelAndView에 addObject로 셋팅 해 준다.
+
 public class TodoController implements Controller {
+
+	Command cmd = new TodoCommand();
 
 	@Override
 	public ModelAndView action(HttpServletRequest req) {
 	
-		// 게시판의 CRUD를 비롯한 세부 기능을 수행하는 Command가 분기된다.
-		// 실행 되는 Command에서 DAO등의 기능을 수행하고 그 결과를 
-		// ModelAndView에 addObject로 셋팅 해 준다.
-		
-		Command cmd = new TodoCommand();
 		ModelAndView mnv = cmd.execute(req);
 		
 		return mnv;
