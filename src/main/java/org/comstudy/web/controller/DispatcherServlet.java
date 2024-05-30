e4025828ae2489b57c6e907e71381a411029fed0
origin <comstudynews@gmail.com>
2024-05-30 11:02:07 +0900

    02 commit day08

---
+++ src/main/java/org/comstudy/web/controller/DispatcherServlet.java
@@ -0,0 +1,34 @@
+package org.comstudy.web.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DispatcherServlet extends HttpServlet {
+	
+	ServletContext context;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		context = config.getServletContext();
+	}
+
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		context.log("process 실행");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+}
