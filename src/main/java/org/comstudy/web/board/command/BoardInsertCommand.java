54c601ef4c1b3c21f4e282294d5ce9962cf1b785
origin <comstudynews@gmail.com>
2024-05-30 14:12:48 +0900

    06 commit day08

---
+++ src/main/java/org/comstudy/web/board/command/BoardInsertCommand.java
@@ -0,0 +1,19 @@
+package org.comstudy.web.board.command;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.comstudy.web.Command;
+import org.comstudy.web.ModelAndView;
+
+public class BoardInsertCommand implements Command {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest req) {
+		System.out.println(">>> BoardCommand 실행");
+		ModelAndView mav = new ModelAndView("board/list");
+		mav.addObject("message", "게시판 실행 결과");
+		mav.addObject("user", "홍길동");
+		
+		return mav;
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -10,7 +10,10 @@
 
 <h1>컴스터디 아카데미</h1>
 <ul>
-	<li><a href="myweb/board/list.do">마이웹-게시판</a></li>
+	<li><a href="myweb/home.do">마이웹-Home</a></li>
+	<li><a href="myweb/board/list.do">마이웹-Board</a></li>
+	<li><a href="myweb/shop/list.do">마이웹-Shop</a></li>
+	<li><a href="myweb/todo/list.do">마이웹-TodoList</a></li>
 </ul>
 
 </body>
